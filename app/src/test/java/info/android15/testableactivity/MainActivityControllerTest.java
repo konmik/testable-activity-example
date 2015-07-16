f6cec0a46ce8c048fb1254ed15895ef0f1aa6873
konmik <morelk@gmail.com>
2015-07-16 21:35:25 +0300

    an additional test

--- app/src/test/java/info/android15/testableactivity/MainActivityControllerTest.java
+++ app/src/test/java/info/android15/testableactivity/MainActivityControllerTest.java
@@ -3,25 +3,42 @@ package info.android15.testableactivity;
 import android.view.View;
 import android.widget.Button;
 
+import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.anyInt;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityControllerTest {
+
+    @Mock MainActivity activity;
+    @Mock Button buttonClickMe;
+
+    MainActivityController controller;
+
+    @Before
+    public void before() {
+        when(activity.findViewById(R.id.buttonClickMe)).thenReturn(buttonClickMe);
+        controller = new MainActivityController(activity);
+    }
 
     @Test
     public void testOnCreate() throws Exception {
-        MainActivity activity = Mockito.mock(MainActivity.class);
-        Button buttonClickMe = Mockito.mock(Button.class);
-        when(activity.findViewById(R.id.buttonClickMe)).thenReturn(buttonClickMe);
-
-        MainActivityController controller = new MainActivityController(activity);
         controller.onCreate(null);
-
         verify(buttonClickMe, times(1)).setOnClickListener(any(View.OnClickListener.class));
+    }
+
+    @Test
+    public void newColor() throws Exception {
+        controller.onCreate(null);
+        controller.onButtonClickMe();
+        verify(buttonClickMe, times(1)).setBackgroundColor(anyInt());
     }
 }
