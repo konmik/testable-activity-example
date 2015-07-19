bc3b2174ea6efd2f017d8b1974f64fc76f1560cf
konmik <morelk@gmail.com>
2015-07-19 17:36:13 +0300

    a more realistic test

--- app/src/test/java/info/android15/testableactivity/MainActivityControllerTest.java
+++ app/src/test/java/info/android15/testableactivity/MainActivityControllerTest.java
@@ -1,5 +1,6 @@
 package info.android15.testableactivity;
 
+import android.app.Activity;
 import android.view.View;
 import android.widget.Button;
 
@@ -7,10 +8,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.stubbing.Answer;
+
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.doAnswer;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -20,25 +26,34 @@ public class MainActivityControllerTest {
 
     @Mock MainActivity activity;
     @Mock Button buttonClickMe;
+    AtomicReference<View.OnClickListener> buttonClickMeOnClickListener;
 
     MainActivityController controller;
 
     @Before
     public void before() {
-        when(activity.findViewById(R.id.buttonClickMe)).thenReturn(buttonClickMe);
+        buttonClickMeOnClickListener = requireOnClick(activity, R.id.buttonClickMe, buttonClickMe);
         controller = new MainActivityController(activity);
-    }
-
-    @Test
-    public void testOnCreate() throws Exception {
         controller.onCreate(null);
-        verify(buttonClickMe, times(1)).setOnClickListener(any(View.OnClickListener.class));
     }
 
     @Test
     public void newColor() throws Exception {
         controller.onCreate(null);
-        controller.onButtonClickMe();
+        buttonClickMeOnClickListener.get().onClick(buttonClickMe);
         verify(buttonClickMe, times(1)).setBackgroundColor(anyInt());
+    }
+
+    public AtomicReference<View.OnClickListener> requireOnClick(Activity activityMock, int viewId, View viewMock) {
+        final AtomicReference<View.OnClickListener> listenerRef = new AtomicReference<>();
+        when(activityMock.findViewById(viewId)).thenReturn(viewMock);
+        doAnswer(new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                listenerRef.set((View.OnClickListener)invocation.getArguments()[0]);
+                return null;
+            }
+        }).when(viewMock).setOnClickListener(any(View.OnClickListener.class));
+        return listenerRef;
     }
 }
